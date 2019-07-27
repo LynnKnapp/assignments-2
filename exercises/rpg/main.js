f0e3fe6d0d8d68111097bb2e6feb3dc31719342a
Lynn Knapp <ebonyeek@example.com>
2019-07-26 20:54:01 -0600

    rpg

--- exercises/rpg/main.js
+++ exercises/rpg/main.js
@@ -56,7 +56,6 @@ let soupNazi = new jerkStore("Soup Nazi", 100, 15, ["crab bisque", "chef knife",
 let frankCostanza = new jerkStore("Frank Costanza", 110, 10, ["frank's cooking", "his obscenities", "headslap"], "You want a piece of me, you want a piece of me.  You got it!!", "Oh no it's Frank Costanza.  Prepare for the feats of strength.")
 
 const jerkStores = [newman, soupNazi, frankCostanza]
-console.log(jerkStores)
 
 let playerNames = player.map(player => player.name)
 let options = readlineSync.keyInSelect(playerNames, "Which player do you choose?")
@@ -70,101 +69,21 @@ if (options === 0) {
     console.log("You've selected Kramer")
 }
 
-if (ready === "yes"){ 
-    while (player.health > 0) {
-        const walk = readline.keyInYN ("Do you want to walk?")   
-        // console.log(walk) 
-        if (walk) {
-            console.log("Your now walking.")
-            let encounter =jerkStoreAppears() 
+while (isAlive && !gameOver) {
+    let question = readlineSync.keyIn("Would you like to walk? [y] to walk, [n] to end game. ", {
+        limit: 'yn'
+    })
+        if (question === "n") {
+            isAlive = false
+            console.log("You just fell into the pool and the pool guy never came to save you.  Your dead.")
             
-            if (encounter){
-                let fight =readline.question ("Do you want to fight or run?")
-                console.log (fight + "test1")
-
-                if (fight === "fight"){
-                    let currentJerkIndex = jerkStores[Math.floor(Math.random() *jerkStores.length)]
-                    let currentJerk = jerkStores[currentJerkIndex]
-                    let currentPersonIndex = player[Math.floor(Math.random() * 3)]
-                    let currentPerson = player[currentPersonIndex]
-                    // const randomNum = Math.floor(Math.random() * 3)
-
-                    if (currentJerk === 0) {
-                            console.log ("Draw your sword and attack when " + currentPerson.name + " is on its tail!") 
-                            console.log(player)
-                            // console.log(Enemy) refer to bossman1
-                            while(player.health > 0 && currentEnemy.health > 0 ){
-                                // if (player === attackEnemy
-                                attackEnemy(currentEnemy)
-                                attackPerson(currentEnemy)
-                                dropItem(currentEnemy)
-                                console.log(currentEnemy)
-                                console.log(player)
-                            }
-                    }else if (randomNum === 1) {
-                        console.log ("Use your hookshot to draw " + currentEnemy.name + " close,then attack quickly before he kicks your ass.")
-                        while(player.health > 0 && currentEnemy.health > 0 ){
-                            attackEnemy(currentEnemy)
-                            attackPerson(currentEnemy)
-                            dropItem(currentEnemy)
-                            console.log(currentEnemy)
-                            console.log(player)
-                        }     
-                    }else if (randomNum === 2) {
-                        console.log ("Use your hammer and stun "  + currentEnemy.name + ".  Then attack with your sword.")
-                        while(player.health > 0 && currentEnemy.health > 0 ){
-                            attackEnemy(currentEnemy)
-                            attackPerson(currentEnemy)
-                            dropItem(currentEnemy)
-                            console.log(currentEnemy)
-                            console.log(player)
-                        }
-                    }
-                }
-                else if (run === true){
-                    console.log("You barely escaped.") 
-                }  
-            }
+        }else if (question === "y") {
+            walk()
+            
         }
-
-// while (isAlive && !gameOver) {
-//     let question = readlineSync.keyIn("Would you like to walk? [y] to walk, [n] to end game. ", {
-//         limit: 'yn'
-//     })
-//         if (question === "n") {
-//             isAlive = false
-//             console.log("You just fell into the pool and the pool guy never came to save you.  Your dead.")
-            
-//         }else if (question === "y") {
-//             walk()
-//         console.log("You are walking now.")
-//         }
-//         jerkStoreAppears()
-//         let chooseFight = readlineSync.keyIn("Do you want to fight [f] for fight, [r] to  run?", {
-//             limit: 'fr'})
+        // jerkStoreAppears()
         
-//         if (chooseFight === "f") {
-//                 if(player.hp >0){
-//                     attackPerson()
-//                 }else if (jerkStores.hp > 0)
-//             if (jerkStores === newman) {
-//                 console.log("Hold onto your food, you encountered Newman, he'll starve immediately without his chocolate sauce to dip his Kenny Roger's broccoli in.")
-//             } else if (jerkStores === soupNazi) {
-//                 console.log("You've encountered the Soup Nazi, don't get in the line.  Then he'll go shoot himself.")
-//             } else if (jerkStores === frankCostanza) {
-//                 console.log("Yell Serenity Now, it'll turn him insane and he'll shrink.")
-//             }
-//             while (player.hp > 0) {
-//                    attackJerkStore()
-            
-//                 if (jerkStores.hp > 0) {
-//                     attackPerson()
-//                 } else if (player.hp > 0 && jerkStores.hp <= 0) {
-//                     item.push(currentJerk.item)
-//                 }
-//             }
-//     } 
-    
+}    
 
     
 
@@ -175,39 +94,75 @@ function walk() {
     if (random > 0) {
         console.log("You are walking now.")
     } else {
-        jerkStoreAppears()
+        if(jerkStores.length === 0){
+            gameOver = true
+            console.log('you won the game')
+        }else {
+            jerkStoreAppears()
+            console.log('hit')
+        }
     }
 }
 
 function jerkStoreAppears() {
     let jerkStore = jerkStores[Math.floor(Math.random() * jerkStores.length)]
+    
     console.log(`you have encountered ${jerkStore.name}`)
     console.log(`${jerkStore.encounterTagLine}`)
+    let chooseFight = readlineSync.keyIn("Do you want to fight [f] for fight, [r] to  run?", {
+        limit: 'fr'})
+    
+    if (chooseFight === "f") {
+        // if(player.hp >0){
+        //         attackPerson()yy
+        // }else if (jerkStores.hp > 0){
+            if (jerkStore === newman) {
+            console.log("Hold onto your food, you encountered Newman, he'll starve immediately without his chocolate sauce to dip his Kenny Roger's broccoli in.")
+            } else if (jerkStore === soupNazi) {
+            console.log("You've encountered the Soup Nazi, don't get in the line.  Then he'll go shoot himself.")
+            }else if (jerkStore === frankCostanza) {
+            console.log("Yell Serenity Now, it'll turn him insane and he'll shrink.")
+            }
+            attackJerkStore(jerkStore)
+                // if (jerkStores.hp > 0) {
+                //     attackPerson()
+                // } else if (player.hp > 0 && jerkStores.hp <= 0) {
+                    
+                // }
+                
+                
+        } 
 
 }
 
-function attackJerkStore() {
-    console.log("Attack Jerk Store.")
-    let currentJerk = jerkStores[Math.floor(Math.random() *jerkStores.length)]
-
+function attackJerkStore(jerk) {
+    console.log("Attack Jerkstore.")
+    if (jerkStores.length === 0){
+        gameOver = true
+    }else {
+        // let currentJerk = jerkStores[Math.floor(Math.random() *jerkStores.length)]
+    // console.log(currentJerk)
     let currentPersonIndex = Math.floor(Math.random() * 3)
     let currentPerson = player[currentPersonIndex]
 
-    while (currentJerk.hp > 0) {
+    while (jerk.hp > 0) {
        let  randomWeapon = currentPerson.weapon[Math.floor(Math.random() * 3)]
         if (randomWeapon === currentPerson.weapon[0]) {
-            console.log(`You attacked ${currentJerk.name} with ${currentPerson.weapon[0]}`)
-            currentJerk.hp -= 10
-            console.log(`${currentJerk.name} now has ${currentJerk.hp} hp points`)
+            console.log(`You attacked ${jerk.name} with ${currentPerson.weapon[0]}`)
+            jerk.hp -= 10
+            console.log(`${jerk.name} now has ${jerk.hp} hp points`)
         } else if (randomWeapon === currentPerson.weapon[1]) {
-            console.log(`${currentJerk.name} attacked you with ${currentPerson.weapon[1]}`)
-            currentJerk.hp -= 15
-            console.log(`${currentJerk.name} now has ${currentJerk.hp} hp points`)
+            console.log(`${jerk.name} attacked you with ${currentPerson.weapon[1]}`)
+            jerk.hp -= 15
+            console.log(`${jerk.name} now has ${jerk.hp} hp points`)
         } else if (randomWeapon === currentPerson.weapon[2]) {
-            console.log(`${currentJerk.name} attacked you with ${currentPerson.weapon[2]}`)
-            currentJerk.hp -= 25
-            console.log(`${currentJerk.name} now has ${currentJerk.hp} hp points`)
+            console.log(`${jerk.name} attacked you with ${currentPerson.weapon[2]}`)
+            jerk.hp -= 25
+            console.log(`${jerk.name} now has ${jerk.hp} hp points`)
         }
+    }
+    jerkStores.splice(jerkStores.indexOf(jerk),1)
+    item.push(jerk.item)
     }
 }
 
@@ -223,21 +178,21 @@ function attackPerson(player) {
             console.log(`${currentPerson.name} attacked you with ${jerkStores.weapon[0]}`)
             player.hp -= 10
             console.log(`${currentPerson.name} now has ${currentPerson.hp} hp points`)
-        } else if (jerkWeapon === weapon[1]) {
-            console.log(`${jerkStores.name} attacked you with ${jerkStores.weapon[1]}`)
-            jerkStore.hp -= 15
-            console.log(`${currentPerson.name} now has ${currentPerson.hp} hp points`)
+            } else if (jerkWeapon === weapon[1]) {
+                console.log(`${jerkStores.name} attacked you with ${jerkStores.weapon[1]}`)
+                jerkStore.hp -= 15
+                console.log(`${currentPerson.name} now has ${currentPerson.hp} hp points`)
 
-        } else if (jerkWeapon === [2]) {
-            console.log(`${jerkStores.name} attacked you with ${jerkStores.weapon[2]}`)
-            jerkStore.hp -= 25
-            console.log(`${player.name} now has ${currentPerson.hp} hp points`)
-        } else {
-            console.log('hello')
-        }
+            } else if (jerkWeapon === [2]) {
+                console.log(`${jerkStores.name} attacked you with ${jerkStores.weapon[2]}`)
+                jerkStore.hp -= 25
+                console.log(`${player.name} now has ${currentPerson.hp} hp points`)
+            } else {
+                console.log('hello')
+            }
     }
 }
-
+// console.log(attackPerson)
 
 
 
